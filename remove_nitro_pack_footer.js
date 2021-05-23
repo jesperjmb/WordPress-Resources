e26ca41bbe805bcfdf92e37389478bbebebabcea
jesper-mb <47184741+jesper-mb@users.noreply.github.com>
2021-05-23 14:46:28 +0200

    Added snippet for removing NitroPack footer/branding

---
+++ remove_nitro_pack_footer.js
@@ -0,0 +1,14 @@
+//Add the follow js snippet to remove the NitroPack footer and branding from any page
+//Please only use this snippet for development/staging. While it can be used for a live site consider instead buying the NitroPack Pro version to support the developers.
+
+document.addEventListener("DOMContentLoaded", function () {
+    let divc = document.querySelectorAll('div[style]');
+    for (let i = 0, len = divc.length; i < len; i++) {
+        let actdisplay = window.getComputedStyle(divc[i], null).display;
+        let actclear = window.getComputedStyle(divc[i], null).clear;
+
+        if (actdisplay == 'block' && actclear == 'both') {
+            divc[i].remove();
+        }
+    }
+});
